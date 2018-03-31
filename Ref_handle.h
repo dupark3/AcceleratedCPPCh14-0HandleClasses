f29c439c8221af20b79907cd7da119dd33acc4e3
Du Park <dupark3@gmail.com>
2018-03-31 13:35:59 -0400

    Reference handle class made. Does not copy object, only makes references to the same object

---
+++ Ref_handle.h
@@ -0,0 +1,64 @@
+#include <cstdef> // size_t
+#include <stdexcept> // runtime_error
+
+template <class T>
+class Ref_handle{
+public:
+    Ref_handle() : p(0), refptr(new std::size_t(1)) { }
+    Ref_handle(T* t) : p(t), refptr(new std::size_t(1)) { }
+    Ref_handle(const Ref_handle& h) : p(h.p), refptr(h.refptr) { ++*refptr; }
+    Ref_handle& operator=(const Ref_handle&);
+    ~Ref_handle();
+
+    operator bool() const { return p; }
+    T& operator*() const {
+        if(p) return *p;
+        throw std::runtime_error("unbound Ref_handle");
+    }
+    T* operator->() const {
+        if(p) return p;
+        throw std::runtime_error("unbound Ref_handle");
+    }
+
+private:
+    T* p;
+    std::size_t* refptr; // points to a size_t that stores how many Ref_handle objects point to the same T object
+};
+
+template <class T>
+Ref_handle<T>& Ref_handle<T>::operator=(const Ref_handle& rhs){
+    ++*rhs.refptr;
+    if (--*refptr == 0){
+        delete refptr;
+        delete p;   
+    }
+    p = rhs.p;
+    refptr = rhs.refptr;
+    return *this;
+}
+
+template <class T>
+Ref_handle<T>::~Ref_handle<T>(){
+    if (--*refptr == 0){
+        delete refptr;
+        delete p;
+    } 
+}
+
+/*template <class T>
+Ref_handle<T>& Ref_handle<T>::operator= (const Ref_handle& rhs){
+    if (rhs.p != p){
+        if (*refptr == 1) {
+            delete p;
+            delete refptr;
+        } else {
+            --*refptr;
+        }
+        p = rhs.p;
+        refptr = rhs.refptr;
+        ++*rhs.refptr;
+    }
+
+    return *this;
+}*/
+
